8c8c45bd8471486be951773d04c66aa6320f361c
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2024-01-06 20:36:59 +0100

    Adding ESA data and making scripts for plot

---
+++ scripts-ESA/main.R
@@ -0,0 +1,11 @@
+
+####################################################
+rm(list = ls())
+
+source('packages.R')
+source('functions.R')
+source('load.R')
+source('combine.R')
+source('calc.R')
+source('export.R')
+source('plot.R')
