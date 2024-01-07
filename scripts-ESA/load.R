d5a0c57bbf36d5ec493f57abcd75a9dde4e80f85
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2024-01-07 13:49:06 +0100

    updating ESA plot

--- scripts-ESA/load.R
+++ scripts-ESA/load.R
@@ -2,3 +2,9 @@
  
 df <- read.csv('../data-ESA/ESAdata.csv', sep = ';')
 
+df$exp <- sub('22H', 'Digestate A', df$exp)
+df$exp <- sub('22I', 'Pig A', df$exp)
+df$exp <- sub('22J', 'Digestate B', df$exp)
+df$exp <- sub('22K', 'Pig B', df$exp)
+
+
