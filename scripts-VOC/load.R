76bbf567d67e5ba2fca7c23bcd9f257f49db0f02
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2024-03-18 13:09:33 +0100

    changing 5-methyl-2-nitrophenol to 4-methyl-2-nitrophenol

--- scripts-VOC/load.R
+++ scripts-VOC/load.R
@@ -1,15 +1,15 @@
 
 # NMVOC emission data 
-digA.A <- read.csv('../data-NMVOC/digA.VOC.A.csv')
+digA.A <- read.csv('../data-VOC/digA.VOC.A.csv')
 digA.A$exp <- 'Digestate A'
 
-digB.A <- read.csv('../data-NMVOC/digB.VOC.A.csv')
+digB.A <- read.csv('../data-VOC/digB.VOC.A.csv')
 digB.A$exp <- 'Digestate B'
 
-pigA.A <- read.csv('../data-NMVOC/pigA.VOC.A.csv')
+pigA.A <- read.csv('../data-VOC/pigA.VOC.A.csv')
 pigA.A$exp <- 'Pig A'
 
-pigB.A <- read.csv('../data-NMVOC/pigB.VOC.A.csv')
+pigB.A <- read.csv('../data-VOC/pigB.VOC.A.csv')
 pigB.A$exp <- 'Pig B'
 
 dv <- rbind(digA.A, digB.A, pigA.A, pigB.A)
